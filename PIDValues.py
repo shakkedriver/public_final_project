c5dbc2210a2c31ee9fe9cf387f7634d462b78e9e
shakkedriver <shakked95@gamil.com>
2022-03-15 17:15:05 +0200

    seems like good parameters in PIDValues.py

--- PIDValues.py
+++ PIDValues.py
@@ -4,17 +4,18 @@ from Drone import FRME_Shape
 
 if __name__ == '__main__':
 
-    hi, wi, = 480, 640
 
     #                      P   I  D
-    xPID = PIDModule.PID(0.33, 0, 0, FRME_Shape[0] // 2)  # x direction
-    yPID = PIDModule.PID(0.27, 0, 0.1, FRME_Shape[1] // 2)  # height
-    zPID = PIDModule.PID(0.003, 0, 0.003, 12000, limit=[-20, 15])  # forward and backwards
+    xPID = PIDModule.PID(Kp=0.25, Ki=0, Kd=0,targetVal= FRME_Shape[0] // 2,limit=[-100, 100])  # x direction
+    yPID = PIDModule.PID(1, 0, 0.1, FRME_Shape[1] // 1.9,limit=[-100, 100])  # height
+    zPID = PIDModule.PID(200, 0, 0, 0.15, limit=[-100, 100])  # forward and backwards
 
     drone = Drone()
     detector = drone.detector
+
+
     drone.takeoff()
-    drone.move_up(20)
+    drone.move_up(50)
     while True:
         img = drone.getFrame()
         # img, objects = detector.center_detect(img)
@@ -25,10 +26,13 @@ if __name__ == '__main__':
         zVal = 0
         if bbox:
             [x, y, w, h, cx, cy, area] = bbox
+            z_ratio = area/(FRME_Shape[0]*FRME_Shape[1])
             cx, cy = int(cx), int(cy)
             xVal = int(xPID.update(cx))
             yVal = int(yPID.update(cy))
-            zVal = int(zPID.update(int(area)))
+            zVal = int(zPID.update(z_ratio))
         # drone.send_rc_control(0, - zVal, -yVal, xVal)
-        drone.send_rc_control(0, 0, 0, xVal)
-
+        # drone.send_rc_control(0, zVal, -yVal, xVal)
+        drone.get_height()
+        # drone.send_rc_control(0, -zVal,-yVal, xVal)
+        drone.send_rc_control(0, -zVal, -yVal, xVal)
