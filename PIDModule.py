a1e9f98b766071757434efe16e523c1c2bd24907
shakkedriver <shakked95@gamil.com>
2022-03-23 12:27:49 +0200

    some documention to the pid module added

--- PIDModule.py
+++ PIDModule.py
@@ -3,11 +3,22 @@ import time
 
 
 class PID:
+    """
+    this class implements a pid control module that can get three constants (gains) and a target goal and allow the user
+    to get the value neesery to input to the system in order to stabilize it closer to the target value.
+    you can read more about the pid model here : https://en.wikipedia.org/wiki/PID_controller
+    """
     def __init__(self, Kp, Ki, Kd, targetVal, limit=None):
         """
 
-        :param pidVals: include the gain values of KP, KI, KD
-        :param targetVal: the desired setpoint
+        :param Kp: the size of the part of the adjustments we want to make in each call to update that is proportional to
+        the error
+        :param Ki: the size of the part of the adjustments we want to make in each call to update that is proportional to
+        the integral of the error across time. the idea is that if we are above the goal for a long time the model would
+        return larger adjustments, so we would not be biased to one side for a long time.
+        :param Kd: the size of the part of the adjustments we want to make in each call to update that is proportional to
+        the derivetiv of the error. if the error got bigger we would want to aplay more force back to the system
+        :param targetVal: the desired set point
         :param limit: limitation of the drone
         """
         self.Kp = Kp
