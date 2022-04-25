8180256328a294c3c73ebacb0512ac5a54979d72
shakkedriver <shakked95@gamil.com>
2022-04-25 20:30:43 +0300

    some fieles added

--- test_times.py
+++ test_times.py
@@ -1,13 +1,18 @@
+import cv2
+
 import Drone
 import time
 
 
 def test_computer_drone_computer(drone):
     total = 0
-    number_of_iter = 1000
+    number_of_iter = 50
     for i in range(number_of_iter):
+        im = drone.getFrame()
         t1 = time.time()
-        drone.detector.find_center(drone.getFrame())
+        blob = cv2.dnn.blobFromImage(im, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+        drone.detector.net.setInput(blob)
+        person = drone.detector.net.forward(drone.detector.ln)
         t2 = time.time()
         total += t2 - t1
 
